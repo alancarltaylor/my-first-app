61e16ba26ed2310419bd30cfd537a23f71e583d4
Alan Taylor <alan.taylor@zayo.com>
2017-04-10 14:05:44 -0600

    two way binding using dollar sign event

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: '[app-servers]',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
-
+	allowNewServer = false;
+	serverCreationStatus = 'no server was created';
+	serverName = '';
+  constructor() { 
+  	setTimeout(()=>{
+  		this.allowNewServer = true;
+  	}, 2000)
+  }
   ngOnInit() {
+  }
+
+  onCreateServer(){
+  	this.serverCreationStatus = 'server was created!';
+  }
+
+  onUpdateServerName(event: Event){
+	this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
